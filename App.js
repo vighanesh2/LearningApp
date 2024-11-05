3c81307d768f5542b5719961960fb7d5279a3cd7
Vighanesh <vgaund@nyit.edu>
2024-11-05 18:09:25 -0500

    new message

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ola amigo</Text>
+      <Text>Hi how are you?</Text>
       <StatusBar style="auto" />
     </View>
   );
